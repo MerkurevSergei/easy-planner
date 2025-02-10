c1b3087e734dca0eb51c95464b4eec5284d14cd5
Merkurev Sergei <MerkurevSergei@yandex.ru>
2025-02-11 04:07:11 +0900

    [EAPL-1] Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # easy-planner
 Планирование проектов - это просто
+
+## Настройка инфраструктуры для локального запуска
+* Создать контейнер с зависимостями проекта:
+```shell
+docker-compose -p easy-planner -f .initial/docker-compose.yaml up
+```
---
+++ src/test/java/ru/merkurevsergei/easy_planner/EasyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merkurevsergei.easy_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
