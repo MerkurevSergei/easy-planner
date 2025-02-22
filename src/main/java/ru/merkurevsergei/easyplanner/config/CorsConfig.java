99331fc50523a662f30ed359470468c4033d5be1
Merkurev Sergei <MerkurevSergei@yandex.ru>
2025-02-23 03:57:28 +0900

    [EAPL-7] Add CORS allowed host

--- src/main/java/ru/merkurevsergei/easyplanner/config/CorsConfig.java
+++ src/main/java/ru/merkurevsergei/easyplanner/config/CorsConfig.java
@@ -10,6 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
